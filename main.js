61199a10eda414259b11cb8b714b5179894a64d2
Ahmed Mamdooh <ahmedmamdoohsd@gmail.com>
2023-01-11 19:48:55 +0200

    Second Project 2nd Edition

---
+++ main.js
@@ -0,0 +1,18 @@
+// // Elements
+// let theLinks = document.querySelector(".links");
+// let theLinksUl = document.querySelector(".links ul");
+// let theLinksUlI = document.querySelector(".icon span:nth-child(2)");
+
+// // the links menu Function
+// theLinks.addEventListener("click", function () {
+//   theLinksUl.classList.toggle("view");
+//   theLinksUlI.classList.toggle("full-width");
+// });
+
+// // Quiting from the links menu
+// window.ondblclick = function () {
+//   if (theLinksUl.classList.contains("view")) {
+//     theLinksUl.classList.remove("view");
+//     theLinksUlI.classList.remove("full-width");
+//   }
+// };
